fc0d58798db182a98081b3f2b51f44b8ca776293
liqian <lq1083301982@163.com>
2022-01-03 15:29:32 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,336 @@
 # k8s
+
+该demo用于介绍kubernetes集群如何搭建,资源如何获取,以及提供相关的软件包以及镜像
+
+并且提供了client-go的demo用于调用k8s集群
+
+## 相关软件以及镜像
+
+链接：https://pan.baidu.com/s/1Z52hPgWqYS0qSCTCsbYeKw 
+提取码：wnob
+
+## Centos7搭建kubernetes-v1.23.1集群
+
+由于kubernetes集群要求机器最少两核心以上,而我的云服务器都是1核1GB的,所以这次采用虚拟机搭建kubernetes集群,并且保证每台虚拟机都在2核2GB配置及以上
+
+### ISO镜像信息
+
+镜像为Centos7-1908最小镜像:
+
+![内核信息](./static/system-core.png)
+
+### 安装前准备
+
+#### 解决网络不可达
+
+因为最小镜像安装,并没有打开网卡开关,使用网络时会出现网络不可达报错信息
+
+![network-unreachable](./static/network-unreachable.png)
+
+解决方法:
+
+修改**/etc/sysconfig/network-scripts/ifcfg-ens33**的onboot选项为yes,然后重启网络
+
+ifcfg-ens33这个文件名和你的网卡有关,可能也叫其他名
+
+![onboot](./static/onboot.png)
+
+```bash
+#修改onboot=yes
+vi /etc/sysconfig/network-scripts/ifcfg-ens33
+#重启网络
+service network restart
+```
+
+#### 更换Yum源
+
+如果使用官方源安装软件包过慢的可以选择更换yum源,这里使用的是阿里源
+
+```bash
+#备份原yum源文件
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+#下载阿里镜像源文件
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+#最小镜像如果没有wget工具则使用curl
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+#清除原有缓存
+yum clean all
+#生成新缓存
+yum makecache
+```
+
+#### 修改DNS以及配置静态IP
+
+如果是虚拟机搭建,并且使用NAT网络,并不需要此步骤
+
+```bash
+vi /etc/sysconfig/network-scripts/ifcfg-ens33
+#修改以下内容
+#BOOTPROTO=static
+#IPADDR=xxx.xxx.xx.xxx
+#NETMASK=xxx.xxx.xxx.xxx
+#GATEWAY=xxx.xxx.xxx.xxx
+#DNS1=xxx.xxx.xxx.xxx
+#DNS2=xxx.xxx.xxx.xxx
+
+#重启网络
+systemctl restart NetworkManager
+```
+
+#### 其他工具安装
+
+安装自己喜欢的一些工具,也可以不安
+
+```bash
+#安装网络工具
+yum install net-tools
+#安装vim
+yum install vim
+#安装ssh
+yum install openssh-server
+```
+
+### 安装包的获取
+
+#### 配置EPEL
+
+由于官方仓库的rpm包资源不够丰富,而且为了稳定,大多为老版本,所以安装EPEL,获取最新的软件包
+
+```bash
+#安装epel
+yum install -y epel-release.noarch
+#查看源列表
+yum repolist
+```
+
+#### 安装yum工具
+
+```bash
+#安装yum工具,使用yum-config-manager操作资源库(对资源库的增删改查)
+#资源库即/etc/yum.repos.d/文件夹下的文件
+yum -y install yum-utils
+```
+
+#### Docker 安装包的获取
+
+##### 安装Docker源
+
+```bash
+yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+```
+
+##### 下载Docker相关软件包
+
+```bash
+mkdir ~/docker
+cd ~/docker
+yumdownloader --resolve docker-ce
+```
+
+结果如图:
+
+![docker-rpm-list](./static/docker-rpm-list.png)
+
+#### Kubernetes安装包的获取
+
+##### 配置Kubernetes源
+
+/etc/yum.repos.d/目录下增加 kubernetes.repo 文件
+
+- 如果你可以访问Google的话,文件内容如下
+
+  ```
+  [kubernetes]
+  name=Kubernetes
+  baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+  enabled=1
+  gpgcheck=1
+  repo_gpgcheck=1
+  gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+  ```
+
+- 如果不能Google,则使用阿里源,文件内容如下
+
+  [阿里官方Kubernetes源使用介绍地址](https://developer.aliyun.com/mirror/kubernetes?spm=a2c6h.13651102.0.0.3e221b11HHHf68)
+
+  ```
+  [kubernetes]
+  name=Kubernetes
+  baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+  enabled=1
+  gpgcheck=1
+  repo_gpgcheck=1
+  gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+  ```
+
+##### 下载Kubernetes相关软件包
+
+```bash
+mkdir ~/k8s
+cd ~/k8s
+yumdownloader --resolve kubelet kubeadm kubectl
+
+#若使用阿里镜像由于官网未开放同步方式, 可能会有索引gpg检查失败的情况这时使用 
+yum install -y --nogpgcheck kubelet kubeadm kubectl
+```
+
+结果如图:
+
+![k8s-rpm-list](./static/k8s-rpm-list.png)
+
+### 开始安装
+
+#### 修改主机名
+
+为了区分集群中的每一个节点的名称,建议修改主机名用于区分
+
+```bash
+vim /etc/hostname
+```
+
+主节点:
+
+![master-hostname](./static/master-hostname.png)
+
+从节点:![node1-hostname](./static/node1-hostname.png)
+
+#### 关闭防火墙并且禁用开机自启
+
+```bash
+systemctl stop firewalld  
+systemctl disable firewalld
+```
+
+#### 关闭 selinux
+
+```bash
+sed -i 's/enforcing/disabled/' /etc/selinux/config
+setenfore 0
+```
+
+#### 关闭swap
+
+所有节点修改 /etc/fstab文件,删除或注释swap配置,然后**重启机器**
+
+```bash
+vim /etc/fstab
+```
+
+![swap-config](./static/swap-config.png)
+
+#### 安装Docker
+
+##### 安装Docker-rpm
+
+```bash
+cd ~/docker
+rpm -ivh --replacefiles --replacepkgs *.rpm
+```
+
+结果如下:
+
+![install-k8s-rpm-success](./static/install-docker-rpm-success.png)
+
+##### 启动Docker并设为开机自启
+
+```bash
+systemctl enable docker.service
+systemctl start docker.service
+```
+
+##### 为Docker配置用户组
+
+修改docker的cgroup driver为systemd
+在/etc/docker目录下新增**daemon.json**,文件内容为:
+
+```
+{
+  "exec-opts": ["native.cgroupdriver=systemd"]
+}
+```
+
+```bash
+#添加当前用户到docker用户组
+sudo usermod -aG docker {用户名}
+#配置完成后需要重启docker
+systemctl restart docker
+```
+
+#### 安装Kubernetes
+
+##### 安装Kubernetes-rpm
+
+```bash
+cd ~/k8s
+rpm -ivh --replacefiles --replacepkgs *.rpm
+```
+
+结果如下:
+
+![install-k8s-rpm-success](./static/install-k8s-rpm-success.png)
+
+##### 查看所需镜像并且加载
+
+```bash
+#查看所需镜像
+kubeadm config images list
+```
+
+结果如图:
+
+![k8s-image-list](./static/k8s-image-list.png)
+
+因为ks.gcr.io国内无法访问,所以镜像无法拉取成功
+
+使用我提供的镜像
+
+加载镜像:
+
+```bash
+docker load -i k8s-image.tar
+```
+
+##### 初始化主节点
+
+```bash
+#初始化主节点
+kubeadm init
+```
+
+初始化中如遇报错如图:
+
+![iptables-error](./static/iptables-error.png)
+
+解决办法:
+
+执行以下命令,然后重新初始化
+
+```bash
+echo "1" >/proc/sys/net/bridge/bridge-nf-call-iptables
+```
+
+初始化成功后将打印出join-token,**并且需要拷贝kubeconfig文件**,按照提示执行即可
+
+生成join-token命令
+
+```bash
+kubeadm token create --print-join-command
+```
+
+##### 从节点如何安装
+
+从节点安装步骤和主节点一样,但是无需初始化,只需要执行kubeadm join即可
+
+##### 主节点安装Calico组件
+
+```bash
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+```
+
+### 安装成功
+
+![get-nodes](./static/get-nodes.png)
+
+![get-pods](./static/get-pods.png)
+
+
---
+++ k8s-client/main.go
@@ -0,0 +1,352 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	apps_v1 "k8s.io/api/apps/v1"
+	core_v1 "k8s.io/api/core/v1"
+	storage_v1 "k8s.io/api/storage/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+/*
+   当前使用kubernetes版本: 1.23.1
+   当前使用go版本：1.17.5
+*/
+
+const (
+	TestNamespace           = "k8s-test"      //测试使用的命名空间
+	TestDockerConfigJsonKey = "docker-harbor" //docker仓库密文key
+)
+
+func main() {
+	clientSet := initClient()
+	createOrUpdateNamespace(clientSet)
+	createOrUpdateSecret(clientSet)
+	//createOrUpdateDeployment(clientSet)
+	//createOrUpdateService(clientSet)
+	//createOrUpdateStorage(clientSet)
+	//listNamespace(clientSet)
+	//listSecret(clientSet)
+	//listDeployment(clientSet)
+	//listService(clientSet)
+	//deleteService(clientSet)
+	//deleteSecret(clientSet)
+	//deleteDeployment(clientSet)
+	//deleteNamespace(clientSet)
+}
+
+/*
+   删除Namespace
+*/
+func deleteNamespace(clientSet *kubernetes.Clientset) {
+	client := clientSet.CoreV1().Namespaces()
+	deletePolicy := meta_v1.DeletePropagationForeground
+	err := client.Delete(context.TODO(), TestNamespace, meta_v1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Namespace删除成功")
+}
+
+/*
+   删除Deployment
+*/
+func deleteDeployment(clientSet *kubernetes.Clientset) {
+	client := clientSet.AppsV1().Deployments(TestNamespace)
+	deletePolicy := meta_v1.DeletePropagationForeground
+	err := client.Delete(context.TODO(), "svc-cloud-resourceserver", meta_v1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Deployment删除成功")
+}
+
+/*
+   删除Secret
+*/
+func deleteSecret(clientSet *kubernetes.Clientset) {
+	client := clientSet.CoreV1().Secrets(TestNamespace)
+	deletePolicy := meta_v1.DeletePropagationForeground
+	err := client.Delete(context.TODO(), TestDockerConfigJsonKey, meta_v1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Secret删除成功")
+}
+
+/*
+    删除Service
+    三种删除策略：
+	Orphan：    只删除当前对象，不删除其所管理的资源对象
+	Background：删除之后，所管理的资源对象由GC删除
+	Foreground：删除之前所管理的资源对象必须先删除
+*/
+func deleteService(clientSet *kubernetes.Clientset) {
+	client := clientSet.CoreV1().Services(TestNamespace)
+	deletePolicy := meta_v1.DeletePropagationForeground
+	err := client.Delete(context.TODO(), "svc-cloud-resourceserver", meta_v1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Service删除成功")
+}
+
+/*
+   获取Service列表,若不指定namespace则获取所有的
+*/
+func listService(clientSet *kubernetes.Clientset) {
+	client := clientSet.CoreV1().Services(TestNamespace)
+	serviceList, err := client.List(context.TODO(), meta_v1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	marshal, _ := json.Marshal(serviceList)
+	fmt.Println(string(marshal))
+}
+
+/*
+   获取Deployment列表,若不指定namespace则获取所有的
+*/
+func listDeployment(clientSet *kubernetes.Clientset) {
+	client := clientSet.AppsV1().Deployments(TestNamespace)
+	deploymentList, err := client.List(context.TODO(), meta_v1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	marshal, _ := json.Marshal(deploymentList)
+	fmt.Println(string(marshal))
+}
+
+/*
+	获取Secret列表,若不指定Namespace则获取所有的
+*/
+func listSecret(clientSet *kubernetes.Clientset) {
+	client := clientSet.CoreV1().Secrets(TestNamespace)
+	secretList, err := client.List(context.TODO(), meta_v1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	marshal, _ := json.Marshal(secretList)
+	fmt.Println(string(marshal))
+}
+
+/*
+   获取命名空间列表
+*/
+func listNamespace(clientSet *kubernetes.Clientset) {
+	client := clientSet.CoreV1().Namespaces()
+	namespaceList, err := client.List(context.TODO(), meta_v1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	marshal, _ := json.Marshal(namespaceList)
+	fmt.Println(string(marshal))
+}
+
+/*
+    创建Storage,已存在则更新
+	源码位置:K8s.io/client-go/kubernetes/typed/storage/v1/storageclass.go
+*/
+func createOrUpdateStorage(clientSet *kubernetes.Clientset) {
+	yamlFile, err := ioutil.ReadFile("./yaml/storageclass.yaml")
+	if err != nil {
+		panic(err)
+	}
+	jsonBytes := yaml2Json(yamlFile)
+	storageClass := storage_v1.StorageClass{}
+	err = json.Unmarshal(jsonBytes, &storageClass)
+	if err != nil {
+		panic(err)
+	}
+	client := clientSet.StorageV1().StorageClasses()
+	if _, err := client.Get(context.TODO(), storageClass.ObjectMeta.Name, meta_v1.GetOptions{}); err != nil {
+		if errors.IsNotFound(err) {
+			if _, err := client.Create(context.TODO(), &storageClass, meta_v1.CreateOptions{}); err != nil {
+				panic(err)
+			}
+			fmt.Println("StorageClass创建成功")
+			return
+		}
+		panic(err)
+	}
+	//if _, err := client.Update(context.TODO(), existService, meta_v1.UpdateOptions{}); err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("service更新成功")
+}
+
+/*
+    创建Service,已存在则更新
+	源码位置:K8s.io/client-go/kubernetes/typed/core/v1/service.go
+*/
+func createOrUpdateService(clientSet *kubernetes.Clientset) {
+	yamlFile, err := ioutil.ReadFile("./yaml/service.yaml")
+	if err != nil {
+		panic(err)
+	}
+	jsonBytes := yaml2Json(yamlFile)
+	service := core_v1.Service{}
+	err = json.Unmarshal(jsonBytes, &service)
+	if err != nil {
+		panic(err)
+	}
+	client := clientSet.CoreV1().Services(TestNamespace)
+	existService, err := client.Get(context.TODO(), service.ObjectMeta.Name, meta_v1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			if _, err := client.Create(context.TODO(), &service, meta_v1.CreateOptions{}); err != nil {
+				panic(err)
+			}
+			fmt.Println("Service创建成功")
+			return
+		}
+		panic(err)
+	}
+	if _, err := client.Update(context.TODO(), existService, meta_v1.UpdateOptions{}); err != nil {
+		panic(err)
+	}
+	fmt.Println("service更新成功")
+}
+
+/*
+   创建Deployment,已存在则更新
+   源码位置:K8s.io/client-go/kubernetes/typed/apps/v1/deployment.go
+*/
+func createOrUpdateDeployment(clientSet *kubernetes.Clientset) {
+	yamlFile, err := ioutil.ReadFile("./yaml/deployment.yaml")
+	if err != nil {
+		panic(err)
+	}
+	jsonBytes := yaml2Json(yamlFile)
+	deployment := apps_v1.Deployment{}
+	err = json.Unmarshal(jsonBytes, &deployment)
+	if err != nil {
+		panic(err)
+	}
+	deploymentClient := clientSet.AppsV1().Deployments(TestNamespace)
+	if _, err = deploymentClient.Get(context.TODO(), deployment.ObjectMeta.Name, meta_v1.GetOptions{}); err != nil {
+		if errors.IsNotFound(err) {
+			if _, err := deploymentClient.Create(context.TODO(), &deployment, meta_v1.CreateOptions{}); err != nil {
+				panic(err)
+			}
+			fmt.Println("Deployment创建成功")
+			return
+		}
+		panic(err)
+	}
+	if _, err := deploymentClient.Update(context.TODO(), &deployment, meta_v1.UpdateOptions{}); err != nil {
+		panic(err)
+	}
+	fmt.Println("Deployment更新成功")
+}
+
+/*
+    创建密文,已存在则更新
+	源码位置:K8s.io/client-go/kubernetes/typed/core/v1/secret.go
+*/
+func createOrUpdateSecret(clientSet *kubernetes.Clientset) {
+	secret := core_v1.Secret{
+		TypeMeta: meta_v1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name:      TestDockerConfigJsonKey,
+			Namespace: TestNamespace,
+		},
+		StringData: map[string]string{
+			core_v1.DockerConfigJsonKey:
+			"{\"auths\":{\"https://registry.dockerhubar.com/\":{\"username\":\"admin\",\"password\":\"123456\"}}}",
+		},
+		Type: core_v1.SecretTypeDockerConfigJson,
+	}
+	client := clientSet.CoreV1().Secrets(TestNamespace)
+	if _, err := client.Get(context.TODO(), secret.ObjectMeta.Name, meta_v1.GetOptions{}); err != nil {
+		if errors.IsNotFound(err) {
+			if _, err := client.Create(context.TODO(), &secret, meta_v1.CreateOptions{}); err != nil {
+				panic(err)
+			}
+			fmt.Println("Secret创建成功")
+			return
+		}
+		panic(err)
+	}
+	if _, err := client.Update(context.TODO(), &secret, meta_v1.UpdateOptions{}); err != nil {
+		panic(err)
+	}
+	fmt.Println("Secret更新成功")
+}
+
+/*
+   创建Namespace,已存在则更新
+   源码位置:K8s.io/client-go/kubernetes/typed/core/v1/namespace.go
+*/
+func createOrUpdateNamespace(clientSet *kubernetes.Clientset) {
+	yamlFile, err := ioutil.ReadFile("./yaml/namespace.yaml")
+	if err != nil {
+		panic(err)
+	}
+	jsonBytes := yaml2Json(yamlFile)
+	namespace := core_v1.Namespace{}
+	err = json.Unmarshal(jsonBytes, &namespace)
+	if err != nil {
+		panic(err)
+	}
+	client := clientSet.CoreV1().Namespaces()
+	if _, err = client.Get(context.TODO(), namespace.ObjectMeta.Name, meta_v1.GetOptions{}); err != nil {
+		if errors.IsNotFound(err) {
+			if _, err := client.Create(context.TODO(), &namespace, meta_v1.CreateOptions{}); err != nil {
+				panic(err)
+			}
+			fmt.Println("Namespace创建成功")
+			return
+		}
+		panic(err)
+	}
+	if _, err := client.Update(context.TODO(), &namespace, meta_v1.UpdateOptions{}); err != nil {
+		panic(err)
+	}
+	fmt.Println("Namespace更新成功")
+}
+
+/*
+   读取配置文件并且初始化客户端
+   kubeconfig 默认在主节点 /etc/kubernetes/admin.conf
+   一般在 $HOME/.kube/config 也会复制一份用于身份认证
+*/
+func initClient() *kubernetes.Clientset {
+	var err error
+	kubeConfig, err := ioutil.ReadFile("./config")
+	restConf, err := clientcmd.RESTConfigFromKubeConfig(kubeConfig)
+	clientSet, err := kubernetes.NewForConfig(restConf)
+	if err != nil {
+		panic(err)
+	}
+	return clientSet
+}
+
+/*
+   yaml转json
+*/
+func yaml2Json(yamlBytes []byte) (jsonBytes []byte) {
+	toJSON, err := yaml.ToJSON(yamlBytes)
+	if err != nil {
+		panic(err)
+	}
+	return toJSON
+}
